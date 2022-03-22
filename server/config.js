47716af824b9fdc65a71a19c9e6da0c29c0d0ad1
luan-antonio <46446054+luan-antonio@users.noreply.github.com>
2022-03-22 15:23:34 -0300

    build(config): set project paths

---
+++ server/config.js
@@ -0,0 +1,27 @@
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const curretDir = dirname(fileURLToPath(import.meta.url));
+const root = join(curretDir, "../");
+const audioDirectory = join(root, "audio");
+const songsDirectory = join(audioDirectory, "songs");
+const fxDirectory = join(audioDirectory, "fx");
+const publicDirectory = join(root, "public");
+
+export default {
+  port: process.env.PORT || 3000,
+  dir: {
+    root,
+    audioDirectory,
+    publicDirectory,
+    songsDirectory,
+    fxDirectory,
+  },
+  pages: {
+    homeHTML: "home/index.html",
+    controllerHTML: "controller/index.html",
+  },
+  location: {
+    home: "/home",
+  },
+};
